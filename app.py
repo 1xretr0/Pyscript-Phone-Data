5e72fab53468cae8a7a2a866d4c095077671e817
Retr0 <sebasmorandez@hotmail.com>
2022-07-07 18:04:45 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+# import phonenumbers
+# from phonenumbers import carrier, geocoder, timezone
+
+# # e.g. +522223391262
+# mobileNumber = input("Enter mobile number with country code: ")
+# mobileNumber = phonenumbers.parse(mobileNumber)
+
+# # get timezone of a phone number
+# print(timezone.time_zones_for_number(mobileNumber))
+
+# #get carrier of a phone number
+# print(carrier.name_for_number(mobileNumber, 'en'))
+
+# #get region info
+# print(geocoder.description_for_number(mobileNumber, 'en'))
+
+# # validating phone number
+# print("Valid Mobile Number: ", phonenumbers.is_valid_number(mobileNumber))
+
+# #checking possibility of a number
+# print("Checking possibility of Number: ",
+# phonenumbers.is_possible_number(mobileNumber))
+
+# TODO
+# https://github.com/pyscript/pyscript/blob/main/docs/tutorials/getting-started.md
+# https://pyscript.net/examples/todo.html

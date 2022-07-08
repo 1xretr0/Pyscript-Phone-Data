ba341fdec5494569ea6084f546c2dc121ee10eb9
Retr0 <sebasmorandez@hotmail.com>
2022-07-08 12:48:54 -0500

    working on script adaptation to pyscript

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
-# import phonenumbers
-# from phonenumbers import carrier, geocoder, timezone
+from xml.dom.minidom import Element
+import phonenumbers
+from phonenumbers import carrier, geocoder, timezone
 
-# # e.g. +522223391262
-# mobileNumber = input("Enter mobile number with country code: ")
-# mobileNumber = phonenumbers.parse(mobileNumber)
+phone_number = Element("phone-input")
 
-# # get timezone of a phone number
-# print(timezone.time_zones_for_number(mobileNumber))
+def searchPhoneData(*ags, **kws):
+    # ignore empty phone number
+    if not phone_number.element.value:
+        return None
 
-# #get carrier of a phone number
-# print(carrier.name_for_number(mobileNumber, 'en'))
+    # e.g. +522223391262
+    # mobileNumber = input("Enter mobile number with country code: ")
+    mobileNumber = phonenumbers.parse(phone_number.element.value)
 
-# #get region info
-# print(geocoder.description_for_number(mobileNumber, 'en'))
+    # get timezone of a phone number
+    phone_timezone = timezone.time_zones_for_number(mobileNumber)
 
-# # validating phone number
-# print("Valid Mobile Number: ", phonenumbers.is_valid_number(mobileNumber))
+    # get carrier of a phone number
+    # phone_carrier = carrier.name_for_number(mobileNumber, 'en')
 
-# #checking possibility of a number
-# print("Checking possibility of Number: ",
-# phonenumbers.is_possible_number(mobileNumber))
+    # get region info
+    phone_region = geocoder.description_for_number(mobileNumber, 'en')
 
-# TODO
-# https://github.com/pyscript/pyscript/blob/main/docs/tutorials/getting-started.md
-# https://pyscript.net/examples/todo.html
+    # validating phone number
+    phone_validation = phonenumbers.is_valid_number(mobileNumber)
+
+    # checking possibility of a number
+    phone_possibility = phonenumbers.is_possible_number(mobileNumber)
+
+    pyscript.write('phone', phone_number)
+    pyscript.write('timezone', phone_timezone)
+    pyscript.write('region', phone_region)
+    pyscript.write('valid', phone_validation)
+    pyscript.write('possibility', phone_possibility)
+
+
+def searchPhoneDataEvent(e):
+    if e.key == "Enter":
+        searchPhoneData()
+
+phone_number.element.onkeypress = searchPhoneDataEvent

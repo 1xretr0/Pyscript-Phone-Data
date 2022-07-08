c7715b909dde89bf2781493dce71d08638eb1a8b
Retr0 <sebasmorandez@hotmail.com>
2022-07-08 18:42:29 -0500

    redefined variables removed bad import first working version

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from xml.dom.minidom import Element
 import phonenumbers
 from phonenumbers import carrier, geocoder, timezone
 
@@ -11,24 +10,24 @@ def searchPhoneData(*ags, **kws):
 
     # e.g. +522223391262
     # mobileNumber = input("Enter mobile number with country code: ")
-    mobileNumber = phonenumbers.parse(phone_number.element.value)
+    mobile_number = phonenumbers.parse(phone_number.element.value)
 
     # get timezone of a phone number
-    phone_timezone = timezone.time_zones_for_number(mobileNumber)
+    phone_timezone = timezone.time_zones_for_number(mobile_number)
 
     # get carrier of a phone number
-    # phone_carrier = carrier.name_for_number(mobileNumber, 'en')
+    # phone_carrier = carrier.name_for_number(mobile_number, 'en')
 
     # get region info
-    phone_region = geocoder.description_for_number(mobileNumber, 'en')
+    phone_region = geocoder.description_for_number(mobile_number, 'en')
 
     # validating phone number
-    phone_validation = phonenumbers.is_valid_number(mobileNumber)
+    phone_validation = phonenumbers.is_valid_number(mobile_number)
 
     # checking possibility of a number
-    phone_possibility = phonenumbers.is_possible_number(mobileNumber)
+    phone_possibility = phonenumbers.is_possible_number(mobile_number)
 
-    pyscript.write('phone', phone_number)
+    pyscript.write('phone', phone_number.element.value)
     pyscript.write('timezone', phone_timezone)
     pyscript.write('region', phone_region)
     pyscript.write('valid', phone_validation)
